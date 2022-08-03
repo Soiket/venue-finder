ad17e2868db3e1fca5c0353a8493f7d1e2a5e0c1
soikot <misoiket@gmail.com>
2022-08-03 19:43:05 +0600

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+    
+          
+                'name' => 'Soikot',
+                'email' => 'soiket@outlook.com',
+                'password' => bcrypt('12345678'),
+         
+
+            ]);
+    
+    }
+}
